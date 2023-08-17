5be7659312ae7e509629dfb4f7791564ca8c2f5c
Akinpelumi Taiwo <68600360+Twain97@users.noreply.github.com>
2023-08-17 10:14:41 -1100

    rename vite config base to TodoApp

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/deploying-vite-TodoApp/',
+  base:'/TodoApp',
   plugins: [
     vue(),
   ],
